d4f65371794bc0379ca6f7d441ec0c9de98ed28b
Younes Haidar <youneshaidar02@gmail.com>
2026-04-17 18:45:57 +0300

    created the dictionary function

---
+++ Code/Compress.py
@@ -0,0 +1,8 @@
+def count_frequencies(text):
+    frequencies = {}
+    for char in text:
+        if char in frequencies:
+            frequencies[char] += 1
+        else:
+            frequencies[char] = 1
+    return frequencies
