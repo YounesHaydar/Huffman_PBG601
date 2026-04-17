b914135672cbde6e833dfc318156f6d81fb60fe8
Younes Haidar <youneshaidar02@gmail.com>
2026-04-17 19:05:29 +0300

    feat: added sorted node list generation

--- Code/Compress.py
+++ Code/Compress.py
@@ -6,3 +6,14 @@ def count_frequencies(text):
         else:
             frequencies[char] = 1
     return frequencies
+
+def create_sorted_node_list(frequencies):
+    node_list= [{'symbol' : char, 'frequency' : count} for char, count in frequencies.items()]
+    node_list.sort(key=lambda node:node['frequency'])
+    return node_list
+
+# Temporary Test Code (Delete before submitting)
+test_text = "aaabbc"
+freqs = count_frequencies(test_text)
+nodes = create_sorted_node_list(freqs)
+print(nodes)
